4158817ea68f696908fb9e9eae4c0a1767841913
Jarryd Deane <Jarryd.deane@gmail.com>
2024-03-20 14:12:47 +0200

    Add xml comment about how unit tests are run. Add unit tests with a generated Guid to show that for each unit test a new instance of Calculator is instantiated

--- 1.Fundamentals/tests/Calculator.Tests.Unit/CalculatorTests.cs
+++ 1.Fundamentals/tests/Calculator.Tests.Unit/CalculatorTests.cs
@@ -1,7 +1,13 @@
 namespace Calculator.Tests.Unit;
 
+
+/// <summary>
+/// Each unit test instantiates a new instance of <see cref="CalculatorTests" />
+/// </summary>
 public sealed class CalculatorTests
 {
+    private readonly Guid _guid = Guid.NewGuid();
+
     [Fact]
     public void Add_Should_AddTwoNumbers_When_TwoNumbersAreIntegers()
     {
@@ -13,5 +19,18 @@ public sealed class CalculatorTests
 
         // Assert
         Assert.Equal(9, result);
+    }
+
+    [Fact]
+    public void Subtract_Should_SubtractTwoNumbers_When_TwoNumbersAreIntegers()
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        var result = calculator.Subtract(10, 4);
+
+        // Assert
+        Assert.Equal(6, result);
     }
 }
