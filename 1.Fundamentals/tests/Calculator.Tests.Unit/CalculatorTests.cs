197b2271d50826f5a03014e9f1b285811fb8d90c
Jarryd Deane <Jarryd.deane@gmail.com>
2024-03-20 14:21:03 +0200

    Setting up and cleaning resources synchronously and asynchronously

--- 1.Fundamentals/tests/Calculator.Tests.Unit/CalculatorTests.cs
+++ 1.Fundamentals/tests/Calculator.Tests.Unit/CalculatorTests.cs
@@ -1,12 +1,19 @@
+
 namespace Calculator.Tests.Unit;
 
 
 /// <summary>
 /// Each unit test instantiates a new instance of <see cref="CalculatorTests" />
 /// </summary>
-public sealed class CalculatorTests
+public sealed class CalculatorTests : IDisposable, IAsyncLifetime
 {
-    private readonly Guid _guid = Guid.NewGuid();
+    private readonly Guid _guid;
+
+    // Used to setup anything before running unit tests
+    public CalculatorTests()
+    {
+        _guid = Guid.NewGuid();
+    }
 
     [Fact]
     public void Add_Should_AddTwoNumbers_When_TwoNumbersAreIntegers()
@@ -28,9 +35,30 @@ public sealed class CalculatorTests
         var calculator = new Calculator();
 
         // Act
-        var result = calculator.Subtract(10, 4);
+        var result = calculator.Subtract(10, 5);
 
         // Assert
-        Assert.Equal(6, result);
+        Assert.Equal(5, result);
+    }
+
+    // Used to cleanup any resources
+    public void Dispose()
+    {
+        Console.WriteLine("Cleaning up code!!!");
+    }
+
+    // Used to setup anything asynchronously before running unit tests
+    public async Task InitializeAsync()
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("Running setup asynchronously");
+    }
+
+    // Used to cleanup any resources asynchronously
+    public Task DisposeAsync()
+    {
+        Console.WriteLine("Cleaning up code asynchronously");
+
+        return Task.CompletedTask;
     }
 }
