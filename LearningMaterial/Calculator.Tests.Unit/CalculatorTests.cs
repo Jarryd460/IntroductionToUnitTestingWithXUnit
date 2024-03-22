762114e0ae03a100bb41d75be5f10436f3d9a82b
Jarryd Deane <Jarryd.deane@gmail.com>
2024-03-22 10:26:53 +0200

    Do exercises with everything that was learnt after completing section. Update README.md file with what was done

---
+++ LearningMaterial/Calculator.Tests.Unit/CalculatorTests.cs
@@ -0,0 +1,60 @@
+namespace Calculator.Tests.Unit;
+
+
+public sealed class CalculatorTests
+{
+    private readonly Calculator _calculator = new Calculator();
+
+    [Theory]
+    [InlineData(5, 5, 10)]
+    [InlineData(-5, 5, 0)]
+    [InlineData(-15, -5, -20)]
+    public void Add_Should_AddTwoNumbers_When_TwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        // Act
+        var result = _calculator.Add(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 0)]
+    [InlineData(15, 5, 10)]
+    [InlineData(-5, -5, 0)]
+    [InlineData(-15, -5, -10)]
+    [InlineData(5, 10, -5)]
+    public void Subtract_Should_SubtractTwoNumbers_When_TwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        // Act
+        var result = _calculator.Subtract(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 25)]
+    [InlineData(50, 0, 0)]
+    [InlineData(-5, 5, -25)]
+    public void Multiply_Should_MultiplyTwoNumbers_When_TwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        // Act
+        var result = _calculator.Multiply(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 1)]
+    [InlineData(15, 5, 3)]
+    public void Divide_Should_DivideTwoNumbers_When_TwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        // Act
+        var result = _calculator.Divide(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # IntroductionToUnitTestingWithXUnit
+
+### Description
+An introduction to unit testing with xunit following [Nick Chapsas: Dometrain - From Zero to Hero Unit testing for](C#https://dometrain.com/course/from-zero-to-hero-unit-testing-in-c/).
+
+### Dependencies:
+* Microsoft.NET.Test.Sdk
+* xunit
+* xunit.runner.visualstudio
+
+### Projects
+
+##### Fundamentals
+Learn the basics of unit testing such as:
+* How to write a unit test.
+* How to run the same unit test with different inputs and expected results.
+* How to skip a unit test.
+* How to setup and dispose of resources when running unit test.
+* Naming convention.
+* Structuring of solution.
