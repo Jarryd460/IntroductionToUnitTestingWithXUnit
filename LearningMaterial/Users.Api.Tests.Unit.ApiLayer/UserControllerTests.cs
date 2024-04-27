41845c5fe30fe994817824a93937ca1fe3000ff0
Jarryd Deane <Jarryd.deane@gmail.com>
2024-04-27 15:00:34 +0200

    Add Users.Api.Tests.Unit.ApiLayer project to LearningMaterial folder so unit tests can be written later

---
+++ LearningMaterial/Users.Api.Tests.Unit.ApiLayer/UserControllerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using Users.Api.Contracts;
+using Users.Api.Controllers;
+using Users.Api.Mappers;
+using Users.Api.Models;
+using Users.Api.Services;
+using Xunit;
+
+namespace Users.Api.Tests.Unit.ApiLayer;
+
+public class UserControllerTests
+{
+    private readonly UserController _sut;
+    private readonly IUserService _userService = Substitute.For<IUserService>();
+
+    public UserControllerTests()
+    {
+        _sut = new UserController(_userService);
+    }
+
+    [Fact]
+    public async Task GetById_ReturnOkAndObject_WhenUserExists()
+    {
+        // Arrange
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            FullName = "Nick Chapsas"
+        };
+        _userService.GetByIdAsync(user.Id).Returns(user);
+        var userResponse = user.ToUserResponse();
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetById(user.Id);
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().BeEquivalentTo(userResponse);
+    }
+
+    [Fact]
+    public async Task GetById_ReturnNotFound_WhenUserDoesntExists()
+    {
+        // Arrange
+        _userService.GetByIdAsync(Arg.Any<Guid>()).ReturnsNull();
+
+        // Act
+        var result = (NotFoundResult)await _sut.GetById(Guid.NewGuid());
+
+        // Assert
+        result.StatusCode.Should().Be(404);
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnEmptyList_WhenNoUsersExist()
+    {
+        // Arrange
+        _userService.GetAllAsync().Returns(Enumerable.Empty<User>());
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetAll();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.As<IEnumerable<UserResponse>>().Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnUsersResponse_WhenUsersExist()
+    {
+        // Arrange
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            FullName = "Nick Chapsas"
+        };
+        var users = new[] { user };
+        var usersResponse = users.Select(x => x.ToUserResponse());
+        _userService.GetAllAsync().Returns(users);
+
+        // Act
+        var result = (OkObjectResult)await _sut.GetAll();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+        result.Value.As<IEnumerable<UserResponse>>().Should().BeEquivalentTo(usersResponse);
+    }
+}
