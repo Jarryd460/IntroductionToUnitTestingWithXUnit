6142b2541080f2ce2ba044da43dfb79687f84acb
Jarryd Deane <Jarryd.deane@gmail.com>
2024-04-27 13:22:56 +0200

    Update nuget package references and Add Users.Api.Tests.Unit.Application so exercise tests can be written later

---
+++ LearningMaterial/Users.Api.Tests.Unit.Application/UserServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Users.Api.Logging;
+using Users.Api.Models;
+using Users.Api.Repositories;
+using Users.Api.Services;
+using Xunit;
+
+namespace Users.Api.Tests.Unit.Application;
+
+public class UserServiceTests
+{
+    private readonly UserService _sut;
+    private readonly IUserRepository _userRepository = Substitute.For<IUserRepository>();
+    private readonly ILoggerAdapter<UserService> _logger = Substitute.For<ILoggerAdapter<UserService>>();
+
+    public UserServiceTests()
+    {
+        _sut = new UserService(_userRepository, _logger);
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldReturnEmptyList_WhenNoUsersExist()
+    {
+        // Arrange
+        _userRepository.GetAllAsync().Returns(Enumerable.Empty<User>());
+
+        // Act
+        var result = await _sut.GetAllAsync();
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldReturnUsers_WhenSomeUsersExist()
+    {
+        // Arrange
+        var nickChapsas = new User
+        {
+            Id = Guid.NewGuid(),
+            FullName = "Nick Chapsas"
+        };
+        var expectedUsers = new[]
+        {
+            nickChapsas
+        };
+        _userRepository.GetAllAsync().Returns(expectedUsers);
+
+        // Act
+        var result = await _sut.GetAllAsync();
+
+        // Assert
+        //result.Single().Should().BeEquivalentTo(nickChapsas);
+        result.Should().BeEquivalentTo(expectedUsers);
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldLogMessages_WhenInvoked()
+    {
+        // Arrange
+        _userRepository.GetAllAsync().Returns(Enumerable.Empty<User>());
+
+        // Act
+        await _sut.GetAllAsync();
+
+        // Assert
+        _logger.Received(1).LogInformation(Arg.Is("Retrieving all users"));
+        _logger.Received(1).LogInformation(Arg.Is("All users retrieved in {0}ms"), Arg.Any<long>());
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldLogMessageAndException_WhenExceptionIsThrown()
+    {
+        // Arrange
+        var sqliteException = new SqliteException("Something went wrong", 500);
+        _userRepository.GetAllAsync()
+            .Throws(sqliteException);
+
+        // Act
+        var requestAction = async () => await _sut.GetAllAsync();
+
+        // Assert
+        await requestAction.Should()
+            .ThrowAsync<SqliteException>().WithMessage("Something went wrong");
+        _logger.Received(1).LogError(Arg.Is(sqliteException), Arg.Is("Something went wrong while retrieving all users"));
+    }
+}
