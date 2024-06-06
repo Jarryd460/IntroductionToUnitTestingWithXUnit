aa6a780cf6d9cece497d974cb281504458dda543
Jarryd Deane <jarryd.deane@gmail.com>
2024-06-06 17:38:48 +0200

    Add comment for MyClassFixture The fixture is called once for both the constructor and Dispose method

--- 5.AdvancedTechniques/tests/AdvancedTechniques.Tests.Unit/MyClassFixture.cs
+++ 5.AdvancedTechniques/tests/AdvancedTechniques.Tests.Unit/MyClassFixture.cs
@@ -1,6 +1,14 @@
 namespace AdvancedTechniques.Tests.Unit;
 
-public sealed class MyClassFixture
+public sealed class MyClassFixture : IDisposable
 {
     public Guid Id { get; } = Guid.NewGuid();
+
+    public void Dispose()
+    {
+        // This method gets call once after 
+        // the class has run all it's unit tests.
+        // The same goes for the constructor of this class.
+        // It is common for integration tests.
+    }
 }
